bb614d92dce8cab0f94c1e0d8a8c7a1f6e127bc6
Tu Nguyen Cam <sueruan012@gmail.com>
2021-04-03 23:39:06 +0700

    submit lab08

--- lab06/src/transforms.cpp
+++ lab06/src/transforms.cpp
@@ -12,25 +12,32 @@ Vector2D operator*(const Matrix3x3 &m, const Vector2D &v) {
 }
 
 Matrix3x3 translate(float dx, float dy) {
-	// Part 3: Fill this in.
-
-
-	return Matrix3x3();
+	double mat[9] = {
+		1., 0., dx,
+		0., 1., dy,
+		0., 0., 1.,
+	};
+	return Matrix3x3(mat);
 }
 
 Matrix3x3 scale(float sx, float sy) {
-	// Part 3: Fill this in.
-
-
-	return Matrix3x3();
+	double mat[9] = {
+		sx, 0., 0.,
+		0., sy, 0.,
+		0., 0., 1.,
+	};
+	return Matrix3x3(mat);
 }
 
 // The input argument is in degrees counterclockwise
 Matrix3x3 rotate(float deg) {
-	// Part 3: Fill this in.
-
-
-	return Matrix3x3();
+	double rad = PI * deg / 180;
+	double mat[9] = {
+		cos(rad), -sin(rad), 0.,
+		sin(rad), cos(rad), 0.,
+		0., 0., 1.,
+	};
+	return Matrix3x3(mat);
 }
 
 }
